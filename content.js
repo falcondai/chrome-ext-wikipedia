84696710893480b4d407c933438f06a8d8274547
Falcon Dai <me@falcondai.com>
2013-08-24 01:35:19 -0500

    auto-scroll to skip to content

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
+// scroll to content
+if (document.location.hash == '') {
+  $('body').scrollTop($('#content').offset().top);
+}
+
+// TODO large popup images when hover
+
+// TODO summary for internal wikipedia links
+
+// add in-page links to section headlines
 $('.mw-headline[id]').wrap(function () {
   return $('<a>').attr('class', 'ext-inpage-link').attr('href', '#' + $(this).attr('id'));
 });
