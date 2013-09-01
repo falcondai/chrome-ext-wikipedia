e26527278ed77de39bcaccf4cf9ae7f0049f7628
Falcon Dai <me@falcondai.com>
2013-09-01 16:36:04 -0500

    improved enlarged image error handling, + entity excerpt tooltip

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ var popover,
     // right-to-left language pages tend to have images on the left side
     direction = html.attr('dir');
 
-$('#content a img').not('.fullImageLink img').mouseenter(function (event) {
+$('#content a[class!="internal"] img').not('.fullImageLink img').mouseenter(function (event) {
   var imgElmt = $(this),
       src = $(this).attr('src'),
       // enlarge images 2X
@@ -19,16 +19,20 @@ $('#content a img').not('.fullImageLink img').mouseenter(function (event) {
     popover = $('<img>').attr('class', 'ext-popover-image');
     body.append(popover);
   }
-  popover.one('error', function () {
-      // show the full resolution image (which is < 2X larger)
-      $(this).attr('src', src.replace(/\/thumb\//, '/').replace(/\/\d+px-.+/, ''))
-        .removeAttr('width')
-        .removeAttr('height')
-    })
-    .attr('width', this.width * 2)
+  if (!imgElmt.attr('data-large-img-src')) {
+    imgElmt.attr('data-large-img-src', src.replace(widthPattern, '/' + width + 'px-'));
+    popover.one('error', function () {
+        // show the full resolution image (which is < 2X larger)
+        var originalSrc = src.replace(/\/thumb\//, '/').replace(/\/\d+px-.+/, '');
+        $(this).attr('src', originalSrc)
+          .removeAttr('width')
+          .removeAttr('height');
+        imgElmt.attr('data-large-img-src', originalSrc);
+      });
+  }
+  popover.attr('width', this.width * 2)
     .attr('height', this.height * 2)
-    .attr('alt', 'enlarged image')
-    .attr('src', src.replace(widthPattern, '/' + width + 'px-'))
+    .attr('src', imgElmt.attr('data-large-img-src'))
     .show();
 })
 .mousemove(function (event) {
@@ -48,4 +52,20 @@ $('#content a img').not('.fullImageLink img').mouseenter(function (event) {
 // add in-page links to section headlines
 $('.mw-headline[id]').wrap(function () {
   return $('<a>').attr('class', 'ext-inpage-link').attr('href', '#' + $(this).attr('id'));
+});
+
+// show snippet for internal Wikipedia links
+var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&srinfo=suggestion&srprop=snippet&format=json&srsearch=';
+function stripHtmlTags(html) {
+  return html.replace(/<.+?>/g, '');
+}
+var a = $('a[href^="/wiki/"][title][class!="internal"]');
+// a.each(function() {
+a.mouseenter(function() {
+  console.log(this.title);
+  var link = this;
+  $.getJSON(searchUrl + this.title, function (data, ts) {
+    console.log(ts, data.query.search[0].snippet);
+    link.title += ' : ' + stripHtmlTags(data.query.search[0].snippet);
+  });
 });
