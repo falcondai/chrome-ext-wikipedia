29ae319a4dcfef2ab9c02196c45bd207c1e7c6cd
Falcon Dai <me@falcondai.com>
2013-09-01 21:40:48 -0500

    fixed image popover bug, improved entity snippet

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
-// TODO + summary for internal wikipedia links
+// TODO + options for each feature
+// TODO improve page snippet results
 // TODO + network graph with reading progress indicator (body of knowledge)
 
 // large popover images when hover
@@ -14,24 +15,27 @@ $('#content a[class!="internal"] img').not('.fullImageLink img').mouseenter(func
   var imgElmt = $(this),
       src = $(this).attr('src'),
       // enlarge images 2X
-      width = +src.match(widthPattern)[1] * 2;
+      width = Math.min(+src.match(widthPattern)[1] * 2, html.width() - 30);
   if (popover == undefined) {
     popover = $('<img>').attr('class', 'ext-popover-image');
     body.append(popover);
   }
   if (!imgElmt.attr('data-large-img-src')) {
     imgElmt.attr('data-large-img-src', src.replace(widthPattern, '/' + width + 'px-'));
-    popover.one('error', function () {
+    popover.unbind('error');
+    popover.bind('error', function () {
+        $(this).unbind('error');
         // show the full resolution image (which is < 2X larger)
         var originalSrc = src.replace(/\/thumb\//, '/').replace(/\/\d+px-.+/, '');
-        $(this).attr('src', originalSrc)
-          .removeAttr('width')
-          .removeAttr('height');
+        $(this).attr('src', originalSrc);
+          // .removeAttr('width')
+          // .removeAttr('height');
         imgElmt.attr('data-large-img-src', originalSrc);
       });
   }
-  popover.attr('width', this.width * 2)
-    .attr('height', this.height * 2)
+  popover
+    // .attr('width', this.width * 2)
+    // .attr('height', this.height * 2)
     .attr('src', imgElmt.attr('data-large-img-src'))
     .show();
 })
@@ -55,17 +59,40 @@ $('.mw-headline[id]').wrap(function () {
 });
 
 // show snippet for internal Wikipedia links
-var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&srinfo=suggestion&srprop=snippet&format=json&srsearch=';
+var wikiApiRoot = '//' + document.location.host + '/w/api.php',
+    searchEndpoint = wikiApiRoot + '?action=query&list=search&srlimit=1&srinfo=suggestion&srprop=snippet&format=json&srsearch=',
+    redirectEndpoint = wikiApiRoot + '?action=query&redirects&format=json&titles=';
+
 function stripHtmlTags(html) {
   return html.replace(/<.+?>/g, '');
 }
-var a = $('a[href^="/wiki/"][title][class!="internal"]');
-// a.each(function() {
-a.mouseenter(function() {
-  console.log(this.title);
-  var link = this;
-  $.getJSON(searchUrl + this.title, function (data, ts) {
-    console.log(ts, data.query.search[0].snippet);
-    link.title += ' : ' + stripHtmlTags(data.query.search[0].snippet);
+function addSnippet(linkElmt, entry) {
+  $.getJSON(searchEndpoint + entry, function (data, status) {
+    console.log(status, data);
+    if (status == 'success' && data.query.search.length > 0 && !linkElmt['data-excerpt']) {
+      linkElmt['data-excerpt'] = stripHtmlTags(data.query.search[0].snippet);
+      linkElmt.title += ' : ' + linkElmt['data-excerpt'];
+    }
+    linkElmt['data-requesting'] = false;
   });
+}
+
+$('#content a[href^="/wiki/"][title][class!="internal"]').not('[href^="/wiki/File:"]')
+.mouseover(function() {
+  var linkElmt = this;
+  if (!this['data-excerpt'] && !this['data-requesting']) {
+    this['data-requesting'] = true;
+    if ($(this).hasClass('mw-redirect')) {
+      // redirecting entities
+      $.getJSON(redirectEndpoint + this.title, function (data, status) {
+        console.log('redirect', status, data);
+        if (status == 'success' && data.query.redirects.length > 0) {
+          console.log(data.query.redirects[0].to);
+          addSnippet(linkElmt, data.query.redirects[0].to);
+        }
+      });  
+    } else {
+      addSnippet(this, this.title);
+    }
+  }
 });
